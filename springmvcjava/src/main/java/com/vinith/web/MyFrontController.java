6daa7a3b5835d8f4f1d4b5b0a9d15d6262e4ce68
vineeth <vineethbalaji95@gmail.com>
2019-09-06 11:01:09 +0530

    commit-4

---
+++ springmvcjava/src/main/java/com/vinith/web/MyFrontController.java
@@ -0,0 +1,25 @@
+package com.vinith.web;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyFrontController extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		
+		return new Class[] {MvcConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[] {"/"};  //for all request it will hit here
+	}
+
+}
---
+++ springmvcjava/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<h1>This is JAVA Based Configuration for Spring WebApp</h1>
+<form action="add">
+<input type ="text" name ="t1"><br>
+<input type ="text" name ="t2"><br>
+<input type ="submit">
+</form>
+</body>
+</html>
